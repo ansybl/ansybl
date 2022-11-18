73f8d2fdec5e7c432880a340956dee4c9750c634
Brian Ford <brianfordp@gmail.com>
2022-11-17 19:33:07 -0500

    Setup PagerDuty alerts function and canto consensus address retrieval

--- cmd/init.go
+++ cmd/init.go
@@ -23,7 +23,11 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
 
+	"github.com/PagerDuty/go-pagerduty"
 	"github.com/spf13/cobra"
 )
 
@@ -34,20 +38,32 @@ var initCmd = &cobra.Command{
 	Long: `Enter the following information so we can alert you via PagerDuty when your validator is missing blocks.
 	
 	1) PagerDuty service ID
-	2) PagerDuty escalation policy ID
+	2) PagerDuty email
 	3) PagerDuty API token`,
 	Run: func(cmd *cobra.Command, args []string) {
+		get_consensus_address()
+
 		fmt.Println("Enter your PagerDuty service ID:")
 		var pd_service_id string
 		fmt.Scanln(&pd_service_id)
+		os.Setenv("PD_SERVICE_ID", pd_service_id)
 
-		fmt.Println("Enter your PagerDuty escalation policy ID:")
-		var pd_policy_id string
-		fmt.Scanln(&pd_policy_id)
+		fmt.Println("Enter your email that's associated with your PagerDuty account:")
+		var pd_email string
+		fmt.Scanln(&pd_email)
+		os.Setenv("PD_EMAIL", pd_email)
 
 		fmt.Println("Enter your PagerDuty API token:")
 		var pd_api_key string
 		fmt.Scanln(&pd_api_key)
+		os.Setenv("PD_API_KEY", pd_api_key)
+
+		fmt.Println("Press enter to send a test alert.")
+		fmt.Scanln()
+		trigger_alarm()
+		fmt.Println("If an alert got triggered, setup is complete.")
+		fmt.Println("If you did not see an alert come through, please try again.")
+		get_consensus_address()
 	},
 }
 
@@ -63,4 +79,42 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func trigger_alarm() {
+	var email = os.Getenv("PD_EMAIL")
+	var service_id = os.Getenv("PD_SERVICE_ID")
+	var authtoken = os.Getenv("PD_API_KEY")
+
+	client := pagerduty.NewClient(authtoken)
+	service := pagerduty.APIReference{
+		ID:   service_id,
+		Type: "service_reference",
+	}
+	body := pagerduty.APIDetails{
+		Type:    "incident_body",
+		Details: "Canto node is missing blocks!",
+	}
+	incident := pagerduty.CreateIncidentOptions{
+		Type:    "incident",
+		Title:   "Canto node is missing blocks",
+		Service: &service,
+		Body:    &body,
+	}
+
+	resp, err := client.CreateIncident(email, &incident)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp)
+}
+
+func get_consensus_address() {
+	out, err := exec.Command("cantod tendermint show-address").Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Setenv("CONSENSUS_ADDRESS", string(out))
+	fmt.Println("Retrieved and set consensus address", string(out))
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/ansybl/ansybl-cli
 go 1.18
 
 require (
+	github.com/PagerDuty/go-pagerduty v1.6.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
